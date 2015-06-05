a5d232230cdc37bd19d55b692180a863d3745163
danbondd <danbondd@gmail.com>
2015-06-05 11:59:16 +0100

    Updated travis.yml, added test cases

---
+++ tests/UrlTest.php
@@ -0,0 +1,33 @@
+<?php
+
+class UrlTest extends TestCase {
+
+	public function testHomePage()
+	{
+		$response = $this->call('GET', '/');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testAboutPage()
+	{
+		$response = $this->call('GET', '/about');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testProjectsPage()
+	{
+		$response = $this->call('GET', '/projects');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+	public function testContactPage()
+	{
+		$response = $this->call('GET', '/contact');
+
+		$this->assertEquals(200, $response->getStatusCode());
+	}
+
+}
