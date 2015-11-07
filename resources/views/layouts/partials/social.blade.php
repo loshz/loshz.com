4c113ecf22a82d9bffaf82a96e0ccd1d5f7621b5
danbondd <danbondd@gmail.com>
2015-11-07 10:54:11 +0000

    Added GA events to contact page

--- resources/views/layouts/partials/social.blade.php
+++ resources/views/layouts/partials/social.blade.php
@@ -1,5 +1,5 @@
 <ul class="social">
-    <li class="github"><a href="https://github.com/danbondd" target="_blank">GitHub</a></li>
-    <li class="twitter"><a href="https://twitter.com/danbondd" target="_blank">Twitter</a></li>
-    <li class="linkedin"><a href="https://www.linkedin.com/in/danbondd" target="_blank">LinkedIn</a></li>
+    <li class="github"><a href="https://github.com/danbondd" target="_blank" class="external" data-source="GitHub">GitHub</a></li>
+    <li class="twitter"><a href="https://twitter.com/danbondd" target="_blank" class="external" data-source="Twitter">Twitter</a></li>
+    <li class="linkedin"><a href="https://www.linkedin.com/in/danbondd" target="_blank" class="external" data-source="LinkedIn">LinkedIn</a></li>
 </ul>
