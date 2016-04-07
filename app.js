6eb1dfe3d0e8019e05128e92a596a71c384ca242
danbondd <danbondd@gmail.com>
2016-04-07 22:23:18 +0100

    Add Sentry error monitoring

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').load();
 
 var express = require('express')
+var raven = require('raven')
 var path = require('path')
 
 var app = express()
 
+app.use(raven.middleware.express.requestHandler(process.env.SENTRY_DSN));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +17,14 @@ app.locals.env = process.env.APP_ENV;
 app.locals.url = process.env.URL;
 
 app.get('/', function (req, res) {
-  res.render('index', {page: 'home'})
+  res.render('index')
 })
 
 app.use(function(req, res, next) {
   res.status(404);
-  res.render('404', {page: 'pagenotfound'});
+  res.render('404');
 });
+
+app.use(raven.middleware.express.errorHandler(process.env.SENTRY_DSN));
  
 app.listen(3030)
