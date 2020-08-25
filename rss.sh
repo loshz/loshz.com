6e46f5c18e2d3717ae92276420012f3afe6eda30
Dan Bond <danbond@protonmail.com>
2020-08-25 16:10:36 +0100

    rss: AWS ELB vs HAProxy

---
+++ rss.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# <p> -> &lt;p&gt;
+sed -i 's/<p>/\&lt\;p\&gt\;/g' index.xml
+
+# </p> -> &lt;/p&gt;
+sed -i 's/<\/p>/\&lt\;\/p\&gt\;/g' index.xml
+
+# <h3> -> &lt;h3&gt;
+sed -i 's/<h3>/\&lt\;h3\&gt\;/g' index.xml
+
+# </h3> -> &lt;/h3&gt;
+sed -i 's/<\/h3>/\&lt\;\/h3\&gt\;/g' index.xml
+
+# <a href=" -> &lt;a href=&quot;
+sed -i 's/<a\ href\=\"/\&lt\;a\ href\=\&quot\;/g' index.xml
+
+# </a> -> &lt;/a&gt;
+sed -i 's/<\/a>/\&lt\;\/a\&gt\;/g' index.xml
+
+# <img src=" -> &lt;img src=&quot;
+sed -i 's/<img\ src\=\"/\&lt\;img\ src\=\&quot\;/g' index.xml
+
+# title=" -> title=&quot;
+sed -i 's/title\=\"/title\=\&quot\;/g' index.xml
+
+# Manual intervention:
+# - Closing href tags '" />'
+# - Some quote marks: "
